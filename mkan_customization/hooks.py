2d3170265a021f7e2d56e56deb4dd6ce585d1f3a
EmkanDevs <emkan.erpnext@gmail.com>
2025-01-09 15:11:13 +0300

    Migrated Custom Field and Property setter --RP

--- mkan_customization/hooks.py
+++ mkan_customization/hooks.py
@@ -242,3 +242,14 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [
+    {
+        "dt": "Custom Field",
+        "filters": {"module": ["in", ["Mkan Customization"]]},
+    },
+    {
+        "dt": "Property Setter",
+        "filters": {"module": ["in", ["Mkan Customization"]]},
+    }
+]
+
