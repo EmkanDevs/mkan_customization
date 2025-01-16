f5d058e241f3c77cdcaff2cba63561a7621e2872
EmkanDevs <emkan.erpnext@gmail.com>
2025-01-16 09:37:31 +0300

    job opening error ps

--- mkan_customization/custom_hr/doctype/job_opening/job_opening.py
+++ mkan_customization/custom_hr/doctype/job_opening/job_opening.py
@@ -3,7 +3,8 @@
 
 # import frappe
 from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class JobOpening(Document):
+class JobOpening(WebsiteGenerator):
 	pass
