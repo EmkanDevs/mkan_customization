4fe94fa731d107b5bdd3cf19c124245961726c3b
EmkanDevs <emkan.erpnext@gmail.com>
2025-02-11 15:34:56 +0300

    PS new development in petty cash

--- mkan_customization/mkan_customization/doctype/petty_cash_request/petty_cash_request.js
+++ mkan_customization/mkan_customization/doctype/petty_cash_request/petty_cash_request.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2025, Finbyz and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Petty Cash Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Petty Cash Request", {
+    refresh(frm) {
+        frm.set_query("employee", function () {
+            return {
+                filters: {
+                    user_id: frappe.session.user,
+                    docstatus:1
+                }
+            };
+        });
+    }
+});
