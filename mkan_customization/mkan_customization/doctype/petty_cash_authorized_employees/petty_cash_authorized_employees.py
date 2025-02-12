d628da733b5e9a6797ffeff14161145764e26914
EmkanDevs <emkan.erpnext@gmail.com>
2025-02-12 16:08:01 +0300

    PS pet petty cash  changes

--- mkan_customization/mkan_customization/doctype/petty_cash_authorized_employees/petty_cash_authorized_employees.py
+++ mkan_customization/mkan_customization/doctype/petty_cash_authorized_employees/petty_cash_authorized_employees.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2025, Finbyz and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class PettyCashAuthorizedEmployees(Document):
-	pass
+	def validate(self):
+		if self.department:
+			doc = frappe.get_doc("Department", self.department)
+			self.set("head_of_department", [])
+			for row in doc.head_of_department:
+				self.append("head_of_department", {"head_of_department": row.head_of_department})
+			if self.head_of_department and not self.supervisor:
+				self.supervisor = self.head_of_department[0].head_of_department
+
