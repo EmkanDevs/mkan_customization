63f9818b4ff7259289a9db537123825158107630
EmkanDevs <emkan.erpnext@gmail.com>
2025-04-16 10:13:18 +0300

    changes

--- mkan_customization/mkan_customization/doc_events/supplier_quotation.py
+++ mkan_customization/mkan_customization/doc_events/supplier_quotation.py
@@ -1,6 +1,7 @@
 import frappe
 
 def on_cancel(self, method):
+    # if connected RFQ's bid tabulation discussion is created and docstatus is 1
     for row in self.items:
         if row.request_for_quotation:
             data = frappe.get_doc("Bid Tabulation Discussion",{"request_for_quotation":row.request_for_quotation})
