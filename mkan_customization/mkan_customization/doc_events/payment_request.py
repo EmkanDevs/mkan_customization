bdb8d6df994354280d55bccde79f53e67795399a
EmkanDevs <emkan.erpnext@gmail.com>
2025-04-16 10:06:38 +0300

    payment request changes

---
+++ mkan_customization/mkan_customization/doc_events/payment_request.py
@@ -0,0 +1,15 @@
+import frappe
+
+def validate(self, method):
+    if self.party_type == "Supplier" and self.party:
+        missing_fields = []
+
+        if not frappe.db.get_value("Supplier", self.party, "custom_default_bank_name"):
+            missing_fields.append("<span style='color:red;'>Default Bank Name</span>")
+        if not frappe.db.get_value("Supplier", self.party, "custom_default_bank_account"):
+            missing_fields.append("<span style='color:red;'>Default Bank Account</span>")
+        if not frappe.db.get_value("Supplier", self.party, "custom_default_iban_no"):
+            missing_fields.append("<span style='color:red;'>IBAN Number</span>")
+
+        if missing_fields:
+            frappe.msgprint(f"The following supplier bank details are missing: {'<,>'.join(missing_fields)}")
