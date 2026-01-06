e2624d5a668d492b3fa375a76ac9a7f4c8908c6c
EmkanDevs <emkan.erpnext@gmail.com>
2026-01-06 14:30:57 +0300

    Temp Room

--- mkan_customization/mkan_customization/doctype/temp_room_booking/temp_room_booking.js
+++ mkan_customization/mkan_customization/doctype/temp_room_booking/temp_room_booking.js
@@ -1,31 +1,52 @@
 // Copyright (c) 2025, Finbyz and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Temp Room Booking", {
-// 	refresh(frm) {
-
-// 	},
-// });
 
 frappe.ui.form.on("Temp Room Booking", {
-    refresh: function(frm) {
-        frm.add_custom_button(__('Create Payment Request'), function() {
+    refresh(frm) {
+        if (frm.is_new()) return;
+
+        frm.add_custom_button(__('Create Payment Request'), () => {
             frappe.call({
                 method: "mkan_customization.mkan_customization.doctype.temp_room_booking.temp_room_booking.make_trb_payment_request",
-                args: {
-                    docname: frm.doc.name
-                },
-                callback: function(r) {
+                args: { docname: frm.doc.name },
+                callback(r) {
                     if (r.message) {
                         frappe.model.sync(r.message);
-                        frappe.show_alert({
-                            message: __("Payment Request {0} created as Draft", [r.message.name]),
-                            indicator: "green"
-                        });
                         frappe.set_route("Form", "Payment Requester", r.message.name);
                     }
                 }
             });
         }, __("Create"));
+
+        frappe.db.get_value("Payment Requester", {
+            reference_doctype: frm.doctype,
+            reference_name: frm.doc.name
+        }, "name").then(r => {
+            if (r?.message?.name) {
+                frm.add_custom_button(__('Create Purchase Invoice'), () => {
+                    create_trb_purchase_invoice(frm, r.message.name);
+                }, __("Create"));
+            }
+        });
     }
 });
+
+function create_trb_purchase_invoice(frm, payment_requester) {
+    frappe.new_doc('Purchase Invoice', {
+        project: frm.doc.project,
+        supplier: frm.doc.payment_to,
+        due_date: frm.doc.payment_due_date,
+        sector: frm.doc.sector,
+        scope : frm.doc.scope,
+        department : frm.doc.department,
+        section : frm.doc.section,
+        region_location : frm.doc.region_location,
+        cost_center: frm.doc.cost_center,
+        bill_no : frm.doc.invoice_no,
+        bill_date : frm.doc.invoice_date,
+        reference_doctype_payment: "Payment Requester",
+        ref_payment_name: payment_requester,
+        reference_doctype: frm.doctype,
+        reference_name: frm.doc.name,
+
+    });
+}
