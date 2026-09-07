3a61c37fb23c2750254603b66373623bb16a5728
Pal Shah <pal.shah@finbyz.tech>
2026-09-07 16:43:52 +0800

    Refactor: streamline item group queries in Item form

--- mkan_customization/public/js/item.js
+++ mkan_customization/public/js/item.js
@@ -1,13 +1,6 @@
 frappe.ui.form.on('Item', {
-	refresh(frm) {
-		frm.set_query('item_group', () => {
-            return {
-                filters: {
-                    is_group: 0
-                }
-            }
-        })
-        		frm.set_query('custom_item_group_l1', () => {
+    refresh(frm) {
+        frm.set_query('custom_item_group_l1', () => {
             return {
                 filters: {
                     parent_item_group: 'All Item Groups'
@@ -31,23 +24,21 @@ frappe.ui.form.on('Item', {
         frm.set_query('item_group', () => {
             return {
                 filters: {
+                    is_group: 0,
                     parent_item_group: frm.doc.custom_item_group_l3
                 }
             }
         })
         remove_duplicate_description(frm);
-        
-	},
-	onload: function(frm) {
+    },
+    onload: function (frm) {
         remove_duplicate_description(frm);
     }
 })
 
 function remove_duplicate_description(frm) {
-    // Check if description exists and matches item_name
-    if (frm.doc.description && frm.doc.item_name && 
+    if (frm.doc.description && frm.doc.item_name &&
         frm.doc.description.trim() === frm.doc.item_name.trim()) {
-        
         frm.doc.description = '';
         frm.refresh_field('description')
     }
